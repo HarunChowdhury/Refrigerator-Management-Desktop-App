17305829178be28b2bf368529bd00013b67d2fe2
harunchowdhury <trishamomo1@gmail.com>
2016-02-25 12:08:25 +0600

    Version 01

---
+++ Refrigerator Management App/Refrigerator Management App/RefrigeratorManagementAppUI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Refrigerator_Management_App
+{
+    public partial class RefrigeratorManagementAppUI : Form
+    {
+        /// <summary>
+        /// this is  RefrigeratorManagementAppUI
+        /// </summary>
+        public RefrigeratorManagementAppUI()
+        {
+            InitializeComponent();
+        }
+        RefrigeratorManager aRefrigeratorManager = new RefrigeratorManager();
+        
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            aRefrigeratorManager.RefrigeratorMaximumWeightHolder = Convert.ToDouble(maximumWeightTextBox.Text);
+        }
+     
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+
+            aRefrigeratorManager.NumberOfUnit = Convert.ToInt32(numberOfUnitTextBox.Text);
+            aRefrigeratorManager.WeightPerUnit = Convert.ToDouble(weightPerUnitTextBox.Text);
+            try
+            {
+               
+               currentWeightTextBox.Text = aRefrigeratorManager.CurrentWeight.ToString();
+               remainingWeightTextBox.Text = aRefrigeratorManager.RemainingWeight.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Overflow");
+            }
+
+            //if (aRefrigeratorManager.RemainingWeight >= 0)
+            //{
+            //    currentWeightTextBox.Text = aRefrigeratorManager.CurrentWeight.ToString();
+            //    remainingWeightTextBox.Text = aRefrigeratorManager.RemainingWeight.ToString();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Overflow");
+            //}
+        }
+
+    }
+}
---
+++ Refrigerator Management App/Refrigerator Management App/RefrigeratorManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Refrigerator_Management_App
+{
+    public class RefrigeratorManager
+    {
+        private double refrigeratorMaximumWeightHolder;
+        private int numberOfUnit;
+        private double weightPerUnit;
+        private double currentWeight;
+        private double remainingWeight;
+
+       
+
+        public double RefrigeratorMaximumWeightHolder
+        {
+            set { refrigeratorMaximumWeightHolder = value; }
+        }
+        
+        public int NumberOfUnit
+        {
+            set { numberOfUnit = value; }
+        }
+     
+        public double WeightPerUnit
+        {
+            set { weightPerUnit = value; }
+        }
+
+        public double CurrentWeight
+        {
+           
+            get { return numberOfUnit*weightPerUnit; }
+        }
+      
+     
+        public double RemainingWeight
+        {
+            get { return refrigeratorMaximumWeightHolder-(numberOfUnit*weightPerUnit); }
+            
+        }
+
+       //public  void CheckCondition()
+       //{
+       //    double weightToBeAdded = numberOfUnit*weightPerUnit;
+       //    if (CurrentWeight + weightToBeAdded <= RefrigeratorMaximumWeightHolder)
+       //    {
+       //      return  CurrentWeight += weightToBeAdded;
+       //      return RemainingWeight = RefrigeratorMaximumWeightHolder - CurrentWeight;
+       //    }
+
+       //    else
+       //    {
+       //        throw new Exception();
+       //    }
+       //}
+    }
+
+  }
